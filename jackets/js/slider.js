2810a8fec5640d04ece0d6ad30f2ad9f87592222
sergick08 <sergick08@gmail.com>
2017-01-23 19:38:44 +0200

    Initial commit

--- jackets/js/slider.js
+++ jackets/js/slider.js
@@ -1,10 +1,22 @@
 $(document).ready()
 $(window).load(function() {
-  $('.flexslider').flexslider({
+  $('#slide-top').flexslider({
     animation: "slide",
     directionNav: false,
     smoothHeight: false,
     slideshowSpeed: 4000,
     
   });
+  $('#slide-prd').flexslider({
+     animation: "slide",
+      itemWidth: 270,
+      itemMargin: 30,
+      minItems: 4,
+      maxItems: 9,
+      move: 0,
+      controlNav: false,
+
+  });
 });
+
+
